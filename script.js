4deefd240396c42897688723bb17631d86455faf
SHIVAM VISHWAKARMA <77149583+CAPTION-SHIVAM@users.noreply.github.com>
2023-07-10 12:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
+	let binary = '';
+  while (decimal > 0) {
+    const remainder = decimal % 2;
+    binary = remainder + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
   
 }
 
